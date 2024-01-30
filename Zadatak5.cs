b26fa50fe0ea4d2ed5a52380169e3411b6457eb1
Benko3 <152974533+Benko3@users.noreply.github.com>
2024-01-30 13:01:35 +0100

    Add files via upload

---
+++ Zadatak5.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string recenica = "Danas radimo s klasom String";
+            Console.WriteLine("Broj znakova u rečenici: {0}",
+                recenica.Length);
+            Console.ReadLine();
+        }
+    }
+}
