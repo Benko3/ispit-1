7c45205473fd6b1eb1b8bbfe4d8c53fc8e0445d3
Benko3 <152974533+Benko3@users.noreply.github.com>
2024-01-30 13:01:16 +0100

    Add files via upload

---
+++ zadatak1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main()
+        {
+           
+       
+    }
+        int x, y, z;
+
+        Console.WriteLine("Unesite vrijednost stranice x:");
+            x = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Unesite vrijednost stranice y:");
+            y = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Unesite vrijednost stranice z:");
+            z = int.Parse(Console.ReadLine());
+
+            if (x > 0 && y > 0 && z > 0)
+            {
+                if (x + y > z && x + z > y && y + z > x)
+                {
+                    Console.WriteLine("DA.");
+                }
+                else
+                {
+                    Console.WriteLine("NE.");
+                }
+            }
+            else
+{
+    Console.WriteLine("Unesene vrijednosti moraju biti veće od 0.");
+}
